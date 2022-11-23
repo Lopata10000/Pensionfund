c95f31e6bf5018ef6a723aaaeb25fd7f0c352735
fanta <c.fanta.petro@student.uzhnu.edu.ua>
2022-11-23 23:57:14 +0200

    Start

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String command;
+
+        do{
+            String[] options = new String[]{"Салам братку! Чого бажаєш?",
+            "1: Реєстрація",
+            "2: Авторизація",
+            "3: Калькулятор",
+            "4: Вихід"};
+            System.out.println(options);
+
+            command = scanner.nextLine();
+        }
+        while()
+        {
+        }
+        }
     }
 }
