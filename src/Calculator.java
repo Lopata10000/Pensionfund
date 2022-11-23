4750f4813bc922ba57daf2a1bb22225d848e66e1
fanta <c.fanta.petro@student.uzhnu.edu.ua>
2022-11-24 00:47:05 +0200

    Menu

---
+++ src/Calculator.java
@@ -0,0 +1,7 @@
+public class Calculator {
+    public static void Calculations()
+    {
+
+        System.out.println("Ти обрав калькулятор");
+    }
+}
---
+++ src/LogIn.java
@@ -0,0 +1,10 @@
+public class LogIn {
+    public static void Registration()
+    {
+        System.out.println("Ти обрав реєстрацію");
+    }
+    public static void Authorization()
+    {
+        System.out.println("Ти обрав авторизацію");
+    }
+}
