3b4ed107dc32a5924ae64c4867994f5467a1717e
fanta <c.fanta.petro@student.uzhnu.edu.ua>
2022-11-24 01:04:16 +0200

    NormalMenu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        String command;
+
+        do{
+            System.out.println("Салам братку! Чого бажаєш?");
+            System.out.println("1: Авторизуватися");
+            System.out.println("2: Зареєструватися");
+            System.out.println("3: Калькулятор");
+            System.out.println("4: Вихід");
+
+            command = scanner.nextLine();
+
+            switch (command){
+
+                case "1":
+                    LogIn.Registration();
+                    break;
+                case "2":
+                    LogIn.Authorization();
+                    break;
+                case "3":
+                    Calculator.Calculations();
+                    break;
+                case "4":
+                    break;
+
+                default:
+                    System.out.println("Непоняль");
+
+            }
+        }
+
+        while(!command.equals("4"));
     }
 }
