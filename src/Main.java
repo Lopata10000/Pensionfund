96207fb3447a99bff71352ef5e84d6214cebea35
fanta <c.fanta.petro@student.uzhnu.edu.ua>
2022-11-25 00:32:28 +0200

    Registration3

--- src/Main.java
+++ src/Main.java
@@ -19,10 +19,10 @@ public class Main {
             switch (command){
 
                 case "1":
-                    LogIn.registration();
+                    LogIn.authorization();
                     break;
                 case "2":
-                    LogIn.authorization();
+                    LogIn.registration();
                     break;
                 case "3":
                     Calculator.Calculations();
