173f0d78d26a9529addc1c251d73e8f9873c6b12
fanta <c.fanta.petro@student.uzhnu.edu.ua>
2022-11-25 00:51:49 +0200

    Registration3

--- src/LogIn.java
+++ src/LogIn.java
@@ -1,51 +1,49 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.Scanner;
 
 import static java.lang.System.in;
 import static java.lang.System.out;
 
 public class LogIn {
-//    public static void Registration() throws IOException {
-//        String fileName = "Users.txt";
-//        String login;
-//        String password;
-//        BufferedWriter writer = null;
-//        try {
-//            writer = new BufferedWriter(new FileWriter(fileName));
-//            Scanner scanner = new Scanner(in);
-//            out.println("Вибери логін");
-//            login = scanner.nextLine();
-//            out.println("Вибери пароль");
-//            password = scanner.nextLine();
-//            writer.append("\n" + password);
-//            writer.append("\n" + login);
-//        } finally {
-//            if (writer != null) {
-//                writer.close();
-//            }
-//
-//        }
-//    }
+    static final String userData = "Users.txt";
+    static Scanner scanner = new Scanner(in);
+    static String newLogin;
+    static String newPassword;
+    static  String login;
+    static String password;
+    boolean checkData;
     public static void authorization()
     {
-        System.out.println("Ти обрав авторизацію");
+        out.println("Ваш логін:");
+        login = scanner.nextLine();
+        out.println("Ваш пароль");
+        password = scanner.nextLine();
+        try( FileReader reader= new FileReader(userData))
+        {
+            Scanner scan = new Scanner(reader);
+
+            if ( check == Files.lines(Paths.get(FILE_NAME.txt), StandardCharsets.UTF_8).anyMatch("c4ca4238a0b923820dcc509a6f75849b"::equals);)
+
+            reader.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static void registration() {
-                String fileName = "Users.txt";
-                Scanner scanner = new Scanner(in);
-                String login;
-                String password;
+    public static void registration() throws IOException {
 
-                try(BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+                try(BufferedWriter writer = new BufferedWriter(new FileWriter(userData))) {
                     out.println("Вибери логін");
-                    login = scanner.nextLine();
+                    newLogin = scanner.nextLine();
                     out.println("Вибери пароль");
-                    password = scanner.nextLine();
-                    writer.append("\n" + password);
-                    writer.append("\n" + login);
+                    newPassword = scanner.nextLine();
+                    writer.append("\n" + newPassword);
+                    writer.append("\n" + newPassword);
+                    writer.close();
                     out.println("Успішно");
+
                 }
                         catch (Exception exception)
                         {
